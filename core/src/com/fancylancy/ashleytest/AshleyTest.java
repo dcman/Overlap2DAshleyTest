0ffe45ffcf85f01cc813058359fc94b65eaffb2e
SuckIt <justin.johnson.90@my.csun.edu>
2015-09-12 15:13:34 -0700

    Removed stock image and red color

--- core/src/com/fancylancy/ashleytest/AshleyTest.java
+++ core/src/com/fancylancy/ashleytest/AshleyTest.java
@@ -12,16 +12,12 @@ public class AshleyTest extends ApplicationAdapter {
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
 	}
 }
