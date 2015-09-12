4d02e650eaf894a4ef59bfb7f11c0323e3d1c03d
SuckIt <justin.johnson.90@my.csun.edu>
2015-09-12 15:54:24 -0700

    Shows Ball on screen

--- core/src/com/fancylancy/ashleytest/AshleyTest.java
+++ core/src/com/fancylancy/ashleytest/AshleyTest.java
@@ -5,11 +5,23 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.brashmonkey.spriter.Player;
+import com.uwsoft.editor.renderer.SceneLoader;
 
 public class AshleyTest extends ApplicationAdapter {
 
+    private SceneLoader sceneLoader;
+    private Viewport viewport;
+
 	@Override
 	public void create () {
+        viewport = new FitViewport(480, 800);
+        sceneLoader = new SceneLoader();
+        sceneLoader.loadScene("MainScene", viewport);
+
+        
 
 	}
 
@@ -17,5 +29,7 @@ public class AshleyTest extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        sceneLoader.getEngine().update(Gdx.graphics.getDeltaTime());
+
 	}
 }
